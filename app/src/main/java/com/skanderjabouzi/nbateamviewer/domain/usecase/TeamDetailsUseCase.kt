a01097765f106f1012ec8257d6d9caad6043bff5
Skander Jabouzi <skander_jabouzi@manulife.ca>
2021-04-02 17:30:46 -0400

    cleaning code

--- app/src/main/java/com/skanderjabouzi/nbateamviewer/domain/usecase/TeamDetailsUseCase.kt
+++ app/src/main/java/com/skanderjabouzi/nbateamviewer/domain/usecase/TeamDetailsUseCase.kt
@@ -1,13 +1,8 @@
 package com.skanderjabouzi.nbateamviewer.domain.usecase
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.skanderjabouzi.nbateamviewer.data.model.TeamDetails
 import com.skanderjabouzi.nbateamviewer.domain.gateway.TeamDetailsRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import retrofit2.Response
-import java.io.IOException
 
 class TeamDetailsUseCase(val repository: TeamDetailsRepository) : UseCase() {
     val teamDetails = MutableLiveData<TeamDetails>()
@@ -23,7 +18,6 @@ class TeamDetailsUseCase(val repository: TeamDetailsRepository) : UseCase() {
                 getRequestFromApi(repository.getTeamsDetails(id))?.let {
                     when (it) {
                         is ResultState.Success -> {
-                            Log.e("###2", "${it.data}")
                             val details = (it.data as TeamDetails)
                             saveTeamDetailsToDb(details)
                             teamDetails.postValue(details)
